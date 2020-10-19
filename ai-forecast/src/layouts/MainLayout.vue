7510cbab599484934db56077ecbb3c3a21f61021
jaeho hwang <jeho137@naver.com>
2020-10-19 15:28:48 +0900

    login

--- ai-forecast/src/layouts/MainLayout.vue
+++ ai-forecast/src/layouts/MainLayout.vue
@@ -14,6 +14,7 @@
       <q-tabs align="left">
         <q-route-tab to="/store" label="store" />
         <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/login" label="Login" />
       </q-tabs>
     </q-header>
 
