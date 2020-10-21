fb3c3b7ea7d3d096ea81a17e46917fb3eb07e6ee
jaehohwang <jeho137@naver.com>
2020-10-21 13:31:17 +0900

    login change

--- ai-forecast/quasar.conf.js
+++ ai-forecast/quasar.conf.js
@@ -89,15 +89,8 @@ module.exports = function (/* ctx */) {
       // (like functional components as one of the examples),
       // you can manually specify Quasar components/directives to be available everywhere:
       //
-      components: [
-        'QLayout',
-        'QPageout',
-        'QPage',
-        'QToolbar'
-
-
-
-      ],
+      // components: [
+      // ],
       // directives: [],
 
       // Quasar plugins
