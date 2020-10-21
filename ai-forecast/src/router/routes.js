0570d518dd43688f4234a31f65265db37dc89ba8
casiris20 <47557740+casiris20@users.noreply.github.com>
2020-10-21 17:36:05 +0900

    merge 오류 해결용

--- ai-forecast/src/router/routes.js
+++ ai-forecast/src/router/routes.js
@@ -9,16 +9,12 @@ const routes = [
       { path: 'login', component: () => import('pages/login.vue') },
       { path: 'register', component: () => import('pages/register.vue') },
       { path: 'predict_variables', component: () => import('pages/predict_variables.vue') },
-      { path: 'item', component: () => import('pages/item.vue') }
+      { path: 'item', component: () => import('pages/item.vue') },
       { path: "home", component: () => import("pages/home.vue") },
       { path: "", component: () => import("pages/Index.vue") },
       { path: "store", component: () => import("pages/store.vue") },
       { path: "login", component: () => import("pages/login.vue") },
       { path: "register", component: () => import("pages/register.vue") },
-      {
-        path: "predict_variables",
-        component: () => import("pages/predict_variables.vue")
-      },
       { path: "item", component: () => import("pages/item.vue") },
       { path: "Predict", component: () => import("pages/Predict.vue") }
     ]
