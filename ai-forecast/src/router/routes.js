f9535e72355619fc3980f1edbe52beb8118d5e7b
casiris20 <47557740+casiris20@users.noreply.github.com>
2020-10-21 16:41:19 +0900

    merge_try

--- ai-forecast/src/router/routes.js
+++ ai-forecast/src/router/routes.js
@@ -4,19 +4,16 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
 
     children: [
-<<<<<<< HEAD
       { path: '', component: () => import('pages/Index.vue') },
       { path: 'store', component: () => import('pages/store.vue') },
       { path: 'login', component: () => import('pages/login.vue') },
       { path: 'register', component: () => import('pages/register.vue') },
       { path: 'predict_variables', component: () => import('pages/predict_variables.vue') },
       { path: 'item', component: () => import('pages/item.vue') }
-=======
       { path: "home", component: () => import("pages/home.vue") },
       { path: "", component: () => import("pages/Index.vue") },
       { path: "store", component: () => import("pages/store.vue") },
       { path: "login", component: () => import("pages/login.vue") },
-      { path: "main_test", component: () => import("pages/main_test.vue") },
       { path: "register", component: () => import("pages/register.vue") },
       {
         path: "predict_variables",
@@ -24,7 +21,6 @@ const routes = [
       },
       { path: "item", component: () => import("pages/item.vue") },
       { path: "Predict", component: () => import("pages/Predict.vue") }
->>>>>>> afc77df077056b454477fedc7c07a206a255659e
     ]
   },
 
