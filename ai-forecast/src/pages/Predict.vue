7b8bd2423309d865c121e48bac54933b7fb098b4
HONG-ILYOUNG <68258721+HONG-ILYOUNG@users.noreply.github.com>
2020-10-22 14:59:31 +0900

    표 수정

--- ai-forecast/src/pages/Predict.vue
+++ ai-forecast/src/pages/Predict.vue
@@ -97,130 +97,57 @@ export default {
     }
   },
 
-  // data () {
-  //   return {
-  //     columns: [
-  //       {
-  //         name: 'name',
-  //         required: true,
-  //         label: 'Dessert (100g serving)',
-  //         align: 'left',
-  //         field: row => row.name,
-  //         format: val => `${val}`,
-  //         sortable: true
-  //       },
-  //       { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
-  //       { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-  //       { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-  //       { name: 'protein', label: 'Protein (g)', field: 'protein' },
-  //       { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-  //       { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-  //       { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
-  //     ],
-  //     data1 : [
-  //       {
-  //         name: 'Frozen Yogurt',
-  //         calories: 159,
-  //         fat: 6.0,
-  //         carbs: 24,
-  //         protein: 4.0,
-  //         sodium: 87,
-  //         calcium: '14%',
-  //         iron: '1%'
-  //       },
-  //       {
-  //         name: 'Ice cream sandwich',
-  //         calories: 237,
-  //         fat: 9.0,
-  //         carbs: 37,
-  //         protein: 4.3,
-  //         sodium: 129,
-  //         calcium: '8%',
-  //         iron: '1%'
-  //       },
-  //       {
-  //         name: 'Eclair',
-  //         calories: 262,
-  //         fat: 16.0,
-  //         carbs: 23,
-  //         protein: 6.0,
-  //         sodium: 337,
-  //         calcium: '6%',
-  //         iron: '7%'
-  //       },
-  //       {
-  //         name: 'Cupcake',
-  //         calories: 305,
-  //         fat: 3.7,
-  //         carbs: 67,
-  //         protein: 4.3,
-  //         sodium: 413,
-  //         calcium: '3%',
-  //         iron: '8%'
-  //       },
-  //       {
-  //         name: 'Gingerbread',
-  //         calories: 356,
-  //         fat: 16.0,
-  //         carbs: 49,
-  //         protein: 3.9,
-  //         sodium: 327,
-  //         calcium: '7%',
-  //         iron: '16%'
-  //       },
-  //       {
-  //         name: 'Jelly bean',
-  //         calories: 375,
-  //         fat: 0.0,
-  //         carbs: 94,
-  //         protein: 0.0,
-  //         sodium: 50,
-  //         calcium: '0%',
-  //         iron: '0%'
-  //       },
-  //       {
-  //         name: 'Lollipop',
-  //         calories: 392,
-  //         fat: 0.2,
-  //         carbs: 98,
-  //         protein: 0,
-  //         sodium: 38,
-  //         calcium: '0%',
-  //         iron: '2%'
-  //       },
-  //       {
-  //         name: 'Honeycomb',
-  //         calories: 408,
-  //         fat: 3.2,
-  //         carbs: 87,
-  //         protein: 6.5,
-  //         sodium: 562,
-  //         calcium: '0%',
-  //         iron: '45%'
-  //       },
-  //       {
-  //         name: 'Donut',
-  //         calories: 452,
-  //         fat: 25.0,
-  //         carbs: 51,
-  //         protein: 4.9,
-  //         sodium: 326,
-  //         calcium: '2%',
-  //         iron: '22%'
-  //       },
-  //       {
-  //         name: 'KitKat',
-  //         calories: 518,
-  //         fat: 26.0,
-  //         carbs: 65,
-  //         protein: 7,
-  //         sodium: 54,
-  //         calcium: '12%',
-  //         iron: '6%'
-  //       }
-  //     ]
-  //   }
-  // }
+  data () {
+    return {
+      columns: [
+        {
+          name: 'Date',
+          required: true,
+          label: 'Date',
+          align: 'left',
+          field: 'Date',
+          sortable: true
+        },
+        {
+          name: 'Predict_Value',
+          label: '예측 수량',
+          align: 'right',
+          field: 'Predict_Value',
+          sortable: true
+        }
+      ],
+      data1 : [
+        {
+          Date: '2020-10-23',
+          Predict_Value: 118,
+        },
+        {
+          Date: '2020-10-24',
+          Predict_Value: 131,
+        },
+        {
+          Date: '2020-10-25',
+          Predict_Value: 159,
+        },
+        {
+          Date: '2020-10-26',
+          Predict_Value: 182,
+        },
+        {
+          Date: '2020-10-27',
+          Predict_Value: 159,
+        },
+        {
+          Date: '2020-10-28',
+          Predict_Value: 284,
+        },
+        {
+          Date: '2020-10-29',
+          Predict_Value: 568,
+        },
+      ]
+    }
+  }
 }
 </script>
 
