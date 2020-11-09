a22cdabfb38cb924b4c2a031fa2cf2bf6cfb8124
HONG-ILYOUNG <68258721+HONG-ILYOUNG@users.noreply.github.com>
2020-11-09 17:57:12 +0900

    수정

--- ai-forecast/src/pages/Predict.vue
+++ ai-forecast/src/pages/Predict.vue
@@ -1,34 +1,65 @@
 <template>
   <div class="justify-center q-ma-sm">
-    
+
     <div class="row justify-center q-col-gutter-sm q-py-sm">
-      <q-chip size="18px" color="red-6" text-color="white" icon="date_range">
+      <q-chip
+        size="18px"
+        color="red-6"
+        text-color="white"
+        icon="date_range"
+      >
         {{getdate}}
       </q-chip>
-      <q-chip size="18px" color="amber-10" text-color="white" icon="store">
+      <q-chip
+        size="18px"
+        color="amber-10"
+        text-color="white"
+        icon="store"
+      >
         {{getstore}}
       </q-chip>
-      <q-chip size="18px" color="blue-12" text-color="white" icon="fastfood">
+      <q-chip
+        size="18px"
+        color="blue-12"
+        text-color="white"
+        icon="fastfood"
+      >
         {{getproduct}}
       </q-chip>
-      <q-chip size="18px" color="purple-13" text-color="white" icon="notifications_active">
+      <q-chip
+        size="18px"
+        color="purple-13"
+        text-color="white"
+        icon="notifications_active"
+      >
         {{getevent}}
       </q-chip>
-      <q-chip size="18px" color="deep-purple-8" text-color="white" icon="beach_access">
+      <q-chip
+        size="18px"
+        color="deep-purple-8"
+        text-color="white"
+        icon="beach_access"
+      >
         {{getflag}}
       </q-chip>
+       <q-btn
+        color="white"
+        text-color="black"
+        label="예측 정보 불러오기"
+        @click="onPredictClick()"
+      />
     </div>
 
-    <div class="row justify-center q-col-gutter-sm q-py-sm">    
+    <div class="row justify-center q-col-gutter-sm q-py-sm">
       <div class="col-md-4 col-md-6 col-lg-6 col-lg-10 col-xs-12 q-pa-sm">
         <q-table
-        title="주간 예측량"
-        :data="data"
-        :columns="columns"
-        row-key="Date"
-      />
+          title="주간 예측량"
+          :data="data"
+          :columns="columns"
+          row-key="Date"
+        />
       </div>
-      
+
       <div class="col-md-4 col-md-6 col-lg-6 col-lg-10 col-xs-12 q-pa-sm">
         <q-carousel
           v-model="slide"
@@ -41,22 +72,34 @@
           padding
           arrows
           height="700px"
-          width = "500px"
+          width="500px"
           class="bg-primary text-white shadow-1 rounded-borders"
         >
-          <q-carousel-slide name="style" class="column no-wrap flex-center">
-            <bar-chart></bar-chart>
-          </q-carousel-slide>
-          
-          <q-carousel-slide name="tv" class="column no-wrap flex-center">
-            <line-chart></line-chart>
+          <q-carousel-slide
+            name="style"
+            class="column no-wrap flex-center"
+          >
+            <bar-chart :chart-data="datacollectionBar"></bar-chart>
           </q-carousel-slide>
 
-          <q-carousel-slide name="layers" class="column no-wrap flex-center">
+          <q-carousel-slide
+            name="tv"
+            class="column no-wrap flex-center"
+          >
+            <line-chart v-if="loaded" :chart-data="datacollectionLine"></line-chart>
+          </q-carousel-slide>
+
+          <q-carousel-slide
+            name="layers"
+            class="column no-wrap flex-center"
+          >
             <mixed-chart></mixed-chart>
           </q-carousel-slide>
 
-          <q-carousel-slide name="ef" class="column no-wrap flex-center">
+          <q-carousel-slide
+            name="ef"
+            class="column no-wrap flex-center"
+          >
             <pie-chart></pie-chart>
           </q-carousel-slide>
 
@@ -68,121 +111,150 @@
 </template>
 
 <script>
-
-    import BarChart from '../components/charts/BarChart'
-    import PieChart from '../components/charts/PieChart'
-    import LineChart from '../components/charts/LineChart'
-    import MixedChart from '../components/charts/MixedChart'
-    import { LocalStorage } from "quasar";
-
-    export default {
-        name: "Predict",
-        components: {
-          BarChart,
-          LineChart,
-          PieChart,
-          MixedChart
+import PieChart from '../components/charts/PieChart'
+import MixedChart from '../components/charts/MixedChart'
+import { LocalStorage } from "quasar";
+import LineChart from '../components/charts/LineChart.js'
+import BarChart from '../components/charts/BarChart.js'
+export default {
+  name: "Predict",
+  components: {
+    BarChart,
+    LineChart,
+    PieChart,
+    MixedChart
+  },
+  data () {
+    return {
+      datacollectionBar: null,
+      datacollectionLine: null,
+      getdate: '날짜',
+      getstore: '매장명',
+      getproduct: '상품명',
+      getevent: '행사',
+      getflag: '휴무일',
+      slide: 'style',
+      // Table Data
+      columns: [
+        {
+          name: 'Date',
+          required: true,
+          label: 'Date',
+          align: 'left',
+          field: 'Date',
+          sortable: true
         },
-        data () {
-          return {
-            getdate: '날짜',
-            getstore: '매장명',
-            getproduct: '',
-            getevent: '행사',
-            getflag: '휴무일',
-            slide: 'style',
-            
-            // Table Data
-            columns: [
-              {
-                name: 'Date',
-                required: true,
-                label: 'Date',
-                align: 'left',
-                field: 'Date',
-                sortable: true
-              },
-              {
-                name: 'Predict_Value',
-                label: '예측 수량',
-                align: 'right',
-                field: 'Predict_Value',
-                sortable: true
-              }
-            ],
-            data : [
-            {
-              Date: '2020-10-23',
-              Predict_Value: 118,
-            },
-            {
-              Date: '2020-10-24',
-              Predict_Value: 131,
-            },
-            {
-              Date: '2020-10-25',
-              Predict_Value: 159,
-            },
-            {
-              Date: '2020-10-26',
-              Predict_Value: 182,
-            },
-            {
-              Date: '2020-10-27',
-              Predict_Value: 159,
-            },
-            {
-              Date: '2020-10-28',
-              Predict_Value: 284,
-            },
-            {
-              Date: '2020-10-29',
-              Predict_Value: 568,
-            },
-            ]
-
-            // Chart Data
-
-          }
-
-        },
-        methods: {
-          getLocalStoragedate: function () {
-            this.getdate = LocalStorage.getItem("date");
-          },
-
-          getLocalStoragestore: function () {
-            this.getstore = LocalStorage.getItem("store");
-          },
-
-          getLocalStorageproduct: function () {
-            this.getproduct = LocalStorage.getItem("item_1");
-          },
-          
-          getLocalStorageevent: function () {
-            this.getevent = LocalStorage.getItem("event_1");
-          },
-
-          getLocalStorageflag: function () {
-            this.getflag = LocalStorage.getItem("flag");
-          },
-          fillData () {
-            this.datacollection = {
-              labels: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
-              datasets: [
-                {
-                  label: 'Value',
-                  backgroundColor: ['#FA6060', '#FFD85B', '#D8F961', '#81D071', '#8193D5', '#6C349D', '#1D2758'],
-                  // Data for the x-axis of the chart
-                  data: [this.getLocalStorageproduct(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
-                }
-              ]
-            }
-          },
-          getRandomInt () {
-            return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-          }
-
+        {
+          name: 'Predict_Value',
+          label: '예측 수량',
+          align: 'right',
+          field: 'Predict_Value',
+          sortable: true
         }
+      ],
+      data: [
+        {
+          Date: '2020-10-23',
+          Predict_Value: 118,
+        },
+        {
+          Date: '2020-10-24',
+          Predict_Value: 131,
+        },
+        {
+          Date: '2020-10-25',
+          Predict_Value: 159,
+        },
+        {
+          Date: '2020-10-26',
+          Predict_Value: 182,
+        },
+        {
+          Date: '2020-10-27',
+          Predict_Value: 159,
+        },
+        {
+          Date: '2020-10-28',
+          Predict_Value: 284,
+        },
+        {
+          Date: '2020-10-29',
+          Predict_Value: 568,
+        },
+      ]
+      // Chart Data
     }
+  },
+  created () {
+    this.fillDataBar()
+    this.fillDataLine()
+  },
+  methods: {
+    fillDataBar () {
+      this.datacollectionBar = {
+        labels: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+        datasets: [
+          {
+            label: 'Value',
+            backgroundColor: ['#FA6060', '#FFD85B', '#D8F961', '#81D071', '#8193D5', '#6C349D', '#1D2758'],
+            // Data for the x-axis of the chart
+            data: [120, 22, 20, 333, 22, 333, 22]
+          }
+        ]
+      }
+      // BarChart.update();
+    },
+    fillDataLine () {
+      // this.datacollectionLine = {
+      //   labels: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+      //   datasets: [{
+      //     data: [120, 222, 222, 222, 222, 222, 222],
+      //     label: 'Predict Value',
+      //     borderColor: '#34495E',
+      //     fill: false,
+      //     lineTension: 0.9
+      //   }
+      //   ]
+      // }
+      this.datacollectionLine = {
+        labels: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+        datasets: [{
+          data: [120, 22, 33, 44, 1, 33, 222],
+          label: 'Predict Value',
+          borderColor: 'white',
+          fill: false,
+          lineTension: 0.7
+        }
+        ]
+      }
+    },
+
+    onPredictClick: function () {
+      this.getdate = LocalStorage.getItem("date");
+      this.getstore = LocalStorage.getItem("store");
+      this.getproduct = LocalStorage.getItem("item_1");
+      this.getevent = LocalStorage.getItem("event_1");
+      this.getflag = LocalStorage.getItem("flag");
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    },
+    // fillData () {
+    //   this.datacollection = {
+    //     labels: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+    //     datasets: [
+    //       {
+    //         label: 'Value',
+    //         backgroundColor: ['#FA6060', '#FFD85B', '#D8F961', '#81D071', '#8193D5', '#6C349D', '#1D2758'],
+    //         // Data for the x-axis of the chart
+    //         data: [this.getLocalStorageproduct(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+    //       }
+    //     ]
+    //   }
+    // },
+    // getRandomInt () {
+    //   return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    // }
+  }
+}
 </script>
