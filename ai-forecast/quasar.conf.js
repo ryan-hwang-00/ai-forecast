9b5167b12188cfb23a9aa9741466a4eab0b9aa43
jaehohwang <jeho137@naver.com>
2020-11-11 16:15:28 +0900

    map update

--- ai-forecast/quasar.conf.js
+++ ai-forecast/quasar.conf.js
@@ -97,7 +97,8 @@ module.exports = function (/* ctx */) {
         'Notify',
         'Dialog',
         'LocalStorage',
-        'SessionStorage'
+        'SessionStorage',
+
       ]
     },
 
--- ai-forecast/src/pages/map.vue
+++ ai-forecast/src/pages/map.vue
@@ -4,32 +4,61 @@
     <div id = "app" square bordered class="text-grand-hotel text-weight-bolder">
         <h4>위치 선택!</h4>
     </div>
+
       <gmap-map
         class="justify-center items-center"
         :center = "center"
         :zoom = "zoom"
         :options = "options"
-        style = "width: 50vw; height: 500px;"
+        style = "width: 60vw; height: 500px;"
       >
       
-        <gmap-marker
+        <!-- <gmap-marker
             :key="index"
-            v-for="(m, index) in markers"
+            v-for="(m, index) in markers" 
             :position="m.position"
             :clickable="true"
             :draggable="false"
-            @click ="tostore"
-        >
-        </gmap-marker>
+            v-on:click= 'tostore1'
+            :icon="{ url:require('../assets/blue-pushpin.png') }"
+            > -->
 
+          <gmap-marker
+          :position="{lat: 35.1028, lng: 129.0403}"
+          :clickable="true"
+          :draggable="false"
+          v-on:click= 'tostore1'
+          :icon="{ url:require('../assets/blue-pushpin.png') }"
+          ></gmap-marker>
+
+          <gmap-marker
+          :position="{lat: 33.35, lng: 126.28}"
+          :clickable="true"
+          :draggable="false"
+          v-on:click= 'tostore2'
+          :icon="{ url:require('../assets/blue-pushpin.png') }"
+          ></gmap-marker>
+
+          <gmap-marker
+          :position="{lat: 37.487935, lng: 126.857758}"
+          :clickable="true"
+          :draggable="false"
+          v-on:click= 'tostore3'
+          :icon="{ url:require('../assets/blue-pushpin.png') }"
+          >
+        
+          
+        </gmap-marker>
       </gmap-map>
     </div>
   </q-page>
 </template>
 
 <script>
+
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps';
+import GmapCustomMarker from 'vue2-gmap-custom-marker';
 
 
 Vue.use (VueGoogleMaps, {
@@ -39,7 +68,7 @@ Vue.use (VueGoogleMaps, {
   }
 })
 
-
+const mapMarker = require('../assets/blue-pushpin.png');
 export default {
   name: 'PageIndex',
   data () {
@@ -49,43 +78,55 @@ export default {
 
       center: {
         lat: 36,
-        lng: 127
+        lng: 127.5
       },
 
-      zoom: 7,
-    
-      markers:[{
-        position: {lat: 35.1028, lng: 129.0403}
-      }, {
-        position: {lat: 127.269311, lng: 37.413294}
-      }]
-
+      zoom: 6.75,
+      
+      markers: [
+        {
+          position: {
+            lat: 35.1028, 
+            lng: 129.0403,
+           
+          }
+        }
+      ]
     }
   },
 
   methods : {
 
-    tostore: function() {
-       {location.href="http://localhost:8080/#/store";
-      
-          this.$q.dialog({
-          title: '부산!',
-          message: '부산지역입니다.'
-        
-          }).onOk(() => {
-            // console.log('OK')
-          }).onCancel(() => {
-            // console.log('Cancel')
-          }).onDismiss(() => {
-            // console.log('I am triggered on both OK and Cancel')
-          })
-      }
-    }
- }
+    tostore1: function() {
+
+    console.log(this.markers[0].position.lat)
+        location.href="http://localhost:8080/#/store"
+
+        this.$q.dialog({
+        title: '지역선택완료!',
+        message: '부산의 매장입니다.'
+      })
+    },
+
+    tostore2: function() {
+  
+        this.$q.dialog({
+        title: '제주도',
+        message: "서비스 준비중입니다."
+    })
+  },
+
+    tostore3: function() {
+  
+        this.$q.dialog({
+        title: '서울',
+        message: "서비스 준비중입니다."
+    })
+  }
+}
 }
 
 </script>
-
 <style scoped>
 
 body {
