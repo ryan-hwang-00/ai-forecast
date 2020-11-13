0abcc77bab19dbdef240d9a8d48784bbbf623003
ryan-hwang-00 <68258601+ryan-hwang-00@users.noreply.github.com>
2020-11-13 16:42:10 +0900

    texteffect bug fix

--- ai-forecast/src/components/textEffect/TextEffect1.vue
+++ ai-forecast/src/components/textEffect/TextEffect1.vue
@@ -28,37 +28,24 @@ export default {
   font-stretch: normal;
 }
 span {font-family: "paybooc-Bold"}
-
 @keyframes text-in {
   0% {
     transform: translate(0, -20px);
     opacity: 0;
   }
 }
-
 .item {
   display: inline-block;
   min-width: 0.3em;
   font-size: 5.5rem;
   animation: text-in 0.8s cubic-bezier(0.22, 0.15, 0.25, 1.43) 0s backwards;
 }
-
-// .right_img {
-//   color: #34495E;
-// }
-
+.right_img {
+  color: #34495E;
+}
 for co in 0 .. 13 {
   .delay-anime:nth-child({co + 1}) {
-    animation-delay: 0 * 100ms + 200ms;
-  }
-  if co == 0 or co == 7 {
-    .right_img:nth-child({co + 1}) {
-      color: red;
-    }
-  } else {
-    .right_img:nth-child({co + 1})  {
-      color: #34495E;
-    }
+    animation-delay: co * 100ms + 200ms;
   }
   // .delay-color:nth-child({co + 1}) {
   //   color: red;
